e2b35f1111e4abe941c1c6a570f5f5cb6f53e278
LucasfNeves <lucasfarias.fn@gmail.com>
2024-01-11 22:02:13 -0300

    routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,37 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { Dashboard } from '../pages/Dashboard'
+import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp'
+
+import { Private } from './Private'
+import { DefaultLayout } from '../layout/defaultLayout'
+import { Profile } from '../pages/Profile'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/app" element={<DefaultLayout />}>
+        <Route
+          path="dashboard"
+          element={
+            <Private>
+              <Dashboard />
+            </Private>
+          }
+        />
+        <Route
+          path="profile"
+          element={
+            <Private>
+              <Profile />
+            </Private>
+          }
+        />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
